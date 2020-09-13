864da050e9c3cf375c7b3c3d387373788d3c5db5
shpun <shpunishment@gmail.com>
2020-09-13 22:53:28 +0800

    热门商品分页

--- src/views/home/module/Hot.vue
+++ src/views/home/module/Hot.vue
@@ -1,18 +1,26 @@
 <!-- 热门商品 -->
 <template>
     <div class="module-hot">
-        <ul class="bg_fff">
-            <li v-for="(item, index) in goodList" :key="index">
-                <Card 
-                    v-if="!item.flashing" 
-                    :cardInfo="item"
-                ></Card>
-                <CardLimit 
-                    v-if="item.flashing" 
-                    :cardInfo="item"
-                ></CardLimit>
-            </li>
-        </ul>
+        <van-list
+                class="bg_fff"
+                v-model="loading"
+                :finished="finished"
+                finished-text="没有更多了"
+                @load="onLoad"
+        >
+            <ul class="__list bg_fff">
+                <li v-for="(item, index) in goodList" :key="index">
+                    <Card
+                            v-if="!item.flashing"
+                            :cardInfo="item"
+                    ></Card>
+                    <CardLimit
+                            v-if="item.flashing"
+                            :cardInfo="item"
+                    ></CardLimit>
+                </li>
+            </ul>
+        </van-list>
     </div>
 </template>
 
@@ -22,25 +30,37 @@ import CardLimit from '../../card/CardLimit'
 export default {
     data () {
         return {
+            loading: false,
+            finished: false,
             formInline: {
                 offset: 0,
                 limit: 20
             },
-            goodList: []
+            goodList: [],
+            page: {
+                offset: 0,
+                limit: 10
+            }
         }
     },
     components: {
         Card,
         CardLimit
     },
-    mounted(){
-        this.getHotList();
-    },
     methods:{
-        // 获取热门商品列表
-        getHotList(){
-            this.$api.home.getHotProduct(this.formInline).then(res => {
-                this.goodList = res.data.data.list;
+        onLoad() {
+            const _this = this
+            _this.loading = true;
+            _this.$api.home.getHotProduct(_this.page).then(res => {
+                _this.loading = false;
+                if (res.data.data.list.length === 0) {
+                    _this.finished = true;
+                } else {
+                    _this.page.offset = _this.page.offset + _this.page.limit
+                    res.data.data.list.forEach(item => {
+                        _this.goodList.push(item)
+                    })
+                }
             }).catch(e => {
                 console.log(e)
             })
@@ -51,7 +71,7 @@ export default {
 
 <style lang="scss" scoped>
     .module-hot{
-        >ul{
+        .__list{
             border-top: 1px solid rgba(238,238,238,0.8);
             >li{
                 position: relative;
